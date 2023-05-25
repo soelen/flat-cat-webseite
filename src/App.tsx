3eb5119f361613a5cb11a39c20dd4113fede4dc4
flat-cat <flat_cat_coding@pm.me>
2023-05-25 17:34:56 +0200

    added bulma

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bulma/css/bulma.min.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+// Return => Funk. Reaktiven HTML Code generieren der asynchron auf Events reagiert
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+  <header className="card-header">
+    <p className="card-header-title">
+      Card header
+    </p>
+    <button className="card-header-icon" aria-label="more options">
+      <span className="icon">
+        <i className="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </header>
+</div>
   )
 }
 
