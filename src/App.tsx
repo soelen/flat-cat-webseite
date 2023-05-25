5bc5d63216b7e504857517d1e0da1d631943b462
flat-cat <flat_cat_coding@pm.me>
2023-05-25 18:07:47 +0200

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'bulma/css/bulma.min.css'
+//import 'bulma/css/bulma.min.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
 // Return => Funk. Reaktiven HTML Code generieren der asynchron auf Events reagiert
   return (
-    <div className="card">
-  <header className="card-header">
-    <p className="card-header-title">
-      Card header
-    </p>
-    <button className="card-header-icon" aria-label="more options">
-      <span className="icon">
-        <i className="fas fa-angle-down" aria-hidden="true"></i>
-      </span>
-    </button>
-  </header>
-</div>
-  )
-}
+ <Header></Header>
+
+  )}
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Index.html =>     <div id="root"></div>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+// Main = Entry Point
