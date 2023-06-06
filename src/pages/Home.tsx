ebec4b72c234aac8c4ce2ae016dca53bf62e7be9
flat-cat <flat_cat_coding@pm.me>
2023-06-06 13:14:15 +0200

    added favicon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,14 @@ function Home() {
 
 // HomePictures
 return (
-  <div className="columns">
+  <div className="columns ">
 
-    <div className="column">
+    <div className="column is-7> textColumn">
       <h1 className="title has-text-black is-spaced">Hello and Welcome!</h1>
-      <h6 className="subtitle has-text-black is-spaced">Hello and Welcome!</h6>
+      <h6 className="subtitle has-text-black is-spaced">to my Webside</h6>
     </div>
 
-    <div className="column ">
+    <div className="column is-one-third picColumn">
       <figure className="image01">
         <img id="homeimage01" src="/img/home/homepic.png" title="title01"/>
       </figure>
