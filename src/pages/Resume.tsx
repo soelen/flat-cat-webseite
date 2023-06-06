55df6afd89af6edc19e080baa3986c1fd6e09efb
flat-cat <flat_cat_coding@pm.me>
2023-06-06 14:39:11 +0200

    added example resumee

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -6,6 +6,11 @@ function Resume() {
   // Skillbars
   return (
     <div className="container">
+
+<figure className="displayResumee">
+        <embed id="resumee" src="/Resumee/resumee.pdf" type="application/pdf" width="100%" height="600px"  title="resumeeTitle"/>
+</figure>
+
 <h1 className="title has-text-black">My Skills</h1>
 
       <SkillBar title="HTML & CSS " value={43}></SkillBar>
