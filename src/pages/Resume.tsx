386b227bfbbbc5147d82bb0c2c6b85900f28332a
flat-cat <flat_cat_coding@pm.me>
2023-06-09 16:36:45 +0200

    added some resume data

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -5,34 +5,43 @@ function Resume() {
 
   // Skillbars
   return (
-<div className="Home">
-      <section className="introduction py-6">
+<div className="resume">
+      <section className="py-6">
         <div className="container">
           <div className="columns my-0">
             <div className="column is-7 textColumn">
 
-
 <div className="columns">
   <div className="column is-one-third">
+
+{/* _Trainings_________________________________________________*/}
     <h4 className="title is-4 pb-5">Trainings</h4>
       <h4 className="subtitle is-4">2020-2023</h4>
-      <h4 className="subtitle is-4">2009-2013</h4>
+      <h4 className="subtitle is-4 mb-6 pb-6 yb-6">2009-2013</h4>
+ 
 
-<h4 className="title is-4 pb-5 pt-5">Vocational stations</h4>
-      <h4 className="subtitle is-4">2020-2023</h4>
-      <h4 className="subtitle is-4">2020-2023</h4>
-      <h4 className="subtitle is-4">2020-2023</h4>
-      
+
+<section>
+{/* _Berufliche stationen_________________________________________________*/}
+<h4 className="title is-4 pb-5 pt-6">Vocational stations</h4>
+      <h4 className="subtitle is-4">2023-2023</h4>
+      <h4 className="subtitle is-4">2022-2022</h4>
+      <h4 className="subtitle is-4">2019-2020</h4>
+      <h4 className="subtitle is-4">2018-2018</h4>
+      <h4 className="subtitle is-4">2013-2018</h4>
+
+      </section>
       
   </div>
     <div className="column">
 
       <h5 className="subtitle is-5">
     Umschulung zum Fachinformatikerin in Anwendungsentwicklung
-    Comcave, Ludwigshafen
+
       </h5>
     <p className="mb-4"> 
     Comcave, Ludwigshafen
+
     </p>
 
 
@@ -44,8 +53,23 @@ function Resume() {
 
     Zinzendorfschule Königsfeld
     </p>
+
+{/* _________________hier muss space rein?_________________________________*/}
+      <h5 className="subtitle is-5 mt-2">
+    Staatliche Anerkennung zur Kinderpflegerin
+    Erwerb der Mittleren Reife
+    </h5>
+    <p className="mt-1">
+
+    Zinzendorfschule Königsfeld
+    </p>
   
-  
+{/* _Berufliche stationen_________________________________________________*/}
+<h4 className="title is-4 pb-5 pt-6">language skills</h4>
+      <h4 className="subtitle is-4">German : native language English: bla </h4>
+      <h4 className="subtitle is-4">English: B1.Niveau </h4>
+
+
   </div>
 </div>
 
